6d3f8f6b5415382a1b5cda8be571afa7ab258513
Jeff Brooks <jeph.brooks@gmail.com>
2022-01-03 21:21:13 -0600

    Update about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="about">
-      <h1>About Me</h1>
-			<h2>Hi there! I'm Jeffgamedev.</h2>
-			<br>
-            <div class="box">
-            I'm a video game developer living in Omaha NE, USA.
-            <br>
-            <br>I began creating video games and software at the age of 12 using various languages,
-            frameworks, and engines like <a href="https://verge-rpg.com">Verge-RPG</a>, <a href="https://pygame.org">Pygame</a>, and <a href="https://liballeg.org/">Allegro</a>.
-            <br>
-            <br>
-			Some big inspirations of mine are Dune (PC-DOS), Earthbound, Chrono Trigger, and Final Fantasy Tactics.
-			Out of high school I studied graphic design at community college, and then Computer Science at University.
-            <br>
-            <br>
-            Since 2012, I've worked with a variety of companies, publishing games for mobile, PC, and consoles.
-            <br>
-            <br>These days, I mostly enjoy using C#, Godot, and Unity.
-			<br>
-            </div>
+      <br>
+      <b-container>
+		<h2>Hi there! I'm Jeffgamedev.</h2>
+		<br>
+        <p>
+        I'm a video game developer and software engineer living in Omaha NE, USA.
+        <br>
+        <br>I began creating video games and software at the age of 12 using various languages,
+        frameworks, and engines like <a href="https://verge-rpg.com">Verge-RPG</a>, <a href="https://pygame.org">Pygame</a>, and <a href="https://liballeg.org/">Allegro</a>.
+        <br>
+        <br>
+		Some big inspirations of mine are Dune (PC-DOS), Earthbound, Chrono Trigger, and Final Fantasy Tactics.
+		Out of high school I studied graphic design at community college, and then Computer Science at University.
+        <br>
+        <br>
+        Since 2012, I've worked with a variety of companies, publishing games for mobile, PC, and consoles.
+        <br>
+        <br>These days, I mostly enjoy using C#, Godot, and Unity.
+		<br>
+        </p>
+      </b-container>
   </div>
 </template>
 
@@ -32,7 +34,7 @@ export default {
 <style scoped>
 .img
 {
-    max-width: 35%;
+    max-width: 45%;
     -webkit-user-drag: none;
     user-select: none;
     -moz-user-select: none;
